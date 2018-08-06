f3ffd2c1583eb4f19066c6e4a9a3a70210162ed7
tigerstripemedia <info@tigerstripemedia.co.uk>
2018-08-06 10:51:24 +0000

    Member Contact Page Dynamic

--- page-member-contact.php
+++ page-member-contact.php
@@ -30,112 +30,43 @@ get_header();
         <h3>My question is related to...</h3>
         <br>
         <div class="col-md-8 offset-md-2">
-          <div class="accordion" id="accordionExample">
+          
+          <?php $loop = new WP_Query( array( 'post_type' => 'member_contact', 'orderby' => 'post_id', 'order' => 'ASC', 'posts_per_page' => -1 ) ); ?>
+          
+          <div class="accordion" id="accordionContact">
+            
+            <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+          
+            <?php
+             $mem_contact_title  = get_field('mem_contact_title');
+             $mem_contact_desc  = get_field('mem_contact_desc');
+             $mem_contact_shortcode  = get_field('mem_contact_shortcode');
+             $mem_contact_id  = get_field('mem_contact_id');
+             $mem_contact_heading  = get_field('mem_contact_heading');
+            ?>
             
             <div class="card question-card">
 
-              <a class="contact-club-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                <div class="card-header" id="headingOne">
+              <a class="contact-club-link" data-toggle="collapse" data-target="#<?php echo $mem_contact_id; ?>" aria-expanded="false" aria-controls="<?php echo $mem_contact_id; ?>">
+                <div class="card-header" id="<?php echo $mem_contact_heading; ?>">
                   <h5 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                      Squads and Training
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#<?php echo $mem_contact_id; ?>" aria-expanded="false" aria-controls="<?php echo $mem_contact_id; ?>">
+                      <?php echo $mem_contact_title; ?>
                     </button>
                   </h5>
                 </div>
               </a>
 
-              <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div id="<?php echo $mem_contact_id; ?>" class="collapse" aria-labelledby="<?php echo $mem_contact_heading; ?>" data-parent="#accordionContact">
                 <div class="card-body bg-light">
-                  <p class="text-muted">Question about squads and training? Fill in the form below to send a message to Paul Braithwaite.</p>
-                  <form>
-                    <div class="form-group">
-                      <label for="name" class="sr-only">Name</label>
-                      <input type="text" class="form-control" id="name" placeholder="Name">
-                    </div>
-                    <div class="form-group">
-                      <label for="email" class="sr-only">Email address</label>
-                      <input type="email" class="form-control" id="email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                      <label for="message" class="sr-only">Message</label>
-                      <textarea class="form-control" id="message" rows="3" placeholder="Message"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block" name="button">Send</button>
-                    <small class="form-text text-muted">The information you provide will be used solely for correspondence and will be stored securely on a password protected server and device. We will not share your information with any other party. This information will be securely disposed once we no longer need to contact you or upon your request.</small>
-                  </form>
+                  <p class="text-muted"><?php echo $mem_contact_desc; ?></p>
+                  <?php echo $mem_contact_shortcode; ?>
                 </div>
               </div>
             </div>
-
-            <div class="card question-card">
-
-              <a class="contact-club-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                <div class="card-header" id="headingTwo">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      Club Shop
-                    </button>
-                  </h5>
-                </div>
-              </a>
-
-              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                <div class="card-body bg-light">
-                  <p class="text-muted">Question about the club shop? Fill in the form below to send a message to Neil Barker.</p>
-                  <form>
-                    <div class="form-group">
-                      <label for="name" class="sr-only">Name</label>
-                      <input type="text" class="form-control" id="name" placeholder="Name">
-                    </div>
-                    <div class="form-group">
-                      <label for="email" class="sr-only">Email address</label>
-                      <input type="email" class="form-control" id="email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                      <label for="message" class="sr-only">Message</label>
-                      <textarea class="form-control" id="message" rows="3" placeholder="Message"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block" name="button">Send</button>
-                    <small class="form-text text-muted">The information you provide will be used solely for correspondence and will be stored securely on a password protected server and device. We will not share your information with any other party. This information will be securely disposed once we no longer need to contact you or upon your request.</small>
-                  </form>
-                </div>
-              </div>
-            </div>
-
-            <div class="card question-card">
-
-              <a class="contact-club-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                <div class="card-header" id="headingThree">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Competitions and Entries
-                    </button>
-                  </h5>
-                </div>
-              </a>
-
-              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                <div class="card-body bg-light">
-                  <p class="text-muted">Question about competitions and entries? Fill in the form below to send a message to Jayne Thorpe.</p>
-                  <form>
-                    <div class="form-group">
-                      <label for="name" class="sr-only">Name</label>
-                      <input type="text" class="form-control" id="name" placeholder="Name">
-                    </div>
-                    <div class="form-group">
-                      <label for="email" class="sr-only">Email address</label>
-                      <input type="email" class="form-control" id="email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                      <label for="message" class="sr-only">Message</label>
-                      <textarea class="form-control" id="message" rows="3" placeholder="Message"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block" name="button">Send</button>
-                    <small class="form-text text-muted">The information you provide will be used solely for correspondence and will be stored securely on a password protected server and device. We will not share your information with any other party. This information will be securely disposed once we no longer need to contact you or upon your request.</small>
-                  </form>
-                </div>
-              </div>
-            </div>
+            
+            <?php endwhile; ?>
+            
           </div>
         </div>
       </div>
