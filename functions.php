5ae0c911e3e0ec5a0280e550e0297584e1f7d58d
tigerstripemedia <info@tigerstripemedia.co.uk>
2018-08-06 10:17:04 +0000

    Contact Form

--- functions.php
+++ functions.php
@@ -347,3 +347,13 @@ add_filter('admin_footer_text', 'remove_footer_admin');
         return $title;
 
     }
+    
+// Remove paragraph tags from contact form 7
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+// Remove span tags from contact form 7
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+
+    return $content;
+});
--- page-contact.php
+++ page-contact.php
@@ -4,6 +4,7 @@
 */ 
 
 // Custom Fields
+$contact_form_shortcode  = get_field('contact_form_shortcode');
 
 get_header();
 ?>
@@ -22,36 +23,7 @@ get_header();
         <p><strong>Already a member?</strong> - If you are already a CoW member please contact us through the Member Dashboard. You can <a href="/member-dashboard/member-contact">find it here.</a></p>
         <br>
         <div class="col-lg-8 offset-lg-2">
-            <form class="web-form">
-              
-              <div class="form-group">
-                <label for="swimmer-name">Swimmer's Name</label>
-                <input type="text" class="form-control" id="swimmer-name" placeholder="Swimmer">
-              </div>
-              
-              <div class="form-group">
-                <label for="swimmer-dob">Swimmer's Date of Birth</label>
-                <input type="date" class="form-control" id="swimmer-dob">
-              </div>
-              
-              <div class="form-group">
-                <label for="parent-name">Parent's Name</label>
-                <input type="text" class="form-control" id="parent-name" placeholder="Parent">
-              </div>
-              
-              <div class="form-group">
-                <label for="email">Contact Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Email">
-              </div>
-              
-              <div class="form-group">
-                <label for="experience">Swimmer's Experience</label>
-                <textarea class="form-control" id="experience" rows="3" placeholder="Experience"></textarea>          
-              </div>
-              
-              <button type="submit" class="btn btn-primary btn-block entries-form-btn">Submit</button>
-              <small class="text-muted">The information you provide will be used solely for correspondence and will be stored securely on a password protected server and device. We will not share your information with any other party. This information will be securely disposed once we no longer need to contact you or upon your request.</small>
-            </form>
+            <?php echo $contact_form_shortcode; ?>
         </div>
     </div>
 </section>
